ae75da88b369659ca7024eb9d4654d77befe7f7f
Aktosha <istam.2006.01@gmail.com>
2022-11-24 20:19:04 +0300

    my ppp

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from .views import MovieListView
+from . import views
 
 urlpatterns = [
-     path('movie/', MovieListView.as_view()),
-    ]
-
-
-
-
+    path('movie/', MovieListView.as_view()),
+    path("movie/<int:pk>/", views.MovieDetailView.as_view()),
+]
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-from django.views.generic import ListView
 from rest_framework.response import Response
 from .models import Movie, MovieLinks
 from rest_framework.views import APIView
@@ -11,6 +9,14 @@ class MovieListView(APIView):
     def get_movie_list(self, request):
         movie = Movie.objects.filter(draft=False)
         serializer = MovieListSerializer(movie, many=True)
+        return Response(serializer.data)
+
+
+class MovieDetailView(APIView):
+    """Вывод фильма"""
+    def get_movie_output(self, request, pk):
+        movie = Movie.objects.get(id=pk, draft=False)
+        serializer = MovieListSerializer(movie)
         return Response(serializer.data)
 
 
